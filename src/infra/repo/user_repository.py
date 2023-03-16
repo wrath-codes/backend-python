24b0d18d8843612adeacfb19a12f428f041e7bd4
Raphael <raphael@wrathcodes.com>
2023-03-16 16:18:38 -0300

    feat: Implementing select_user in UserRepository

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Users as UserModel
@@ -7,7 +9,7 @@ class UserRepository:
     """Class to manage User repository"""
 
     @classmethod
-    def insert_user(self, name: str, password: str) -> Users:
+    def insert_user(cls, name: str, password: str) -> Users:
         """Insert data in user entity
         :param - name: user name
         :param - password: user password
@@ -28,5 +30,53 @@ class UserRepository:
                 raise
             finally:
                 db_connection.session.close()
+
+        return None
+
+    @classmethod
+    def select_user(cls, user_id: int = None, name: str = None) -> List[Users]:
+        """Select data in user entity by id or name
+        :param - user_id: user's id
+        :param - name: user's  name
+        :return - list with Users selected
+        """
+
+        try:
+            query_data = None
+
+            if user_id and not name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UserModel)
+                        .filter_by(id=user_id)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif not user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UserModel)
+                        .filter_by(name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+            elif user_id and name:
+                with DBConnectionHandler() as db_connection:
+                    data = (
+                        db_connection.session.query(UserModel)
+                        .filter_by(id=user_id, name=name)
+                        .one()
+                    )
+                    query_data = [data]
+
+            return query_data
+
+        except:
+            db_connection.session.rollback()
+            raise
+        finally:
+            db_connection.session.close()
 
         return None
--- src/infra/repo/user_repository_test.py
+++ src/infra/repo/user_repository_test.py
@@ -2,6 +2,7 @@ from faker import Faker
 from sqlalchemy import text
 
 from src.infra.config import DBConnectionHandler
+from src.infra.entities import Users as UsersModel
 
 from .user_repository import UserRepository
 
@@ -33,3 +34,37 @@ def test_insert_user():
     assert new_user.id == query_user.id
     assert new_user.name == query_user.name
     assert new_user.password == query_user.password
+
+
+def test_select_user():
+    """Should select a user in Users table and compare it"""
+
+    user_id = fake.random_number(digits=5)
+    name = fake.name()
+    password = fake.word()
+    data = UsersModel(id=user_id, name=name, password=password)
+
+    engine = db_connection_handler.get_engine()
+
+    with engine.connect() as conn:
+        conn.execute(
+            text(
+                "INSERT INTO users (id, name, password) VALUES ({}, '{}', '{}')".format(
+                    user_id, name, password
+                )
+            )
+        )
+        conn.commit()
+
+    query_user1 = user_repository.select_user(user_id=user_id)
+    query_user2 = user_repository.select_user(name=name)
+    query_user3 = user_repository.select_user(user_id=user_id, name=name)
+
+    assert data in query_user1
+    assert data in query_user2
+    assert data in query_user3
+
+    # Delete User
+    with engine.connect() as conn:
+        conn.execute(text("DELETE FROM users WHERE id = {}".format(user_id)))
+        conn.commit()
